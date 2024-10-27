a929fea53012eadfdf8497b4c42e9406556852cf
siggwer <jerome.sauvage@etu.unilim.fr>
2024-10-27 18:05:50 +0100

    Update projct

--- public/assets/js/main-mention.js
+++ public/assets/js/main-mention.js
@@ -1,4 +1,184 @@
 // Main javascript
+// (function ($) {
+//     "use strict";
+//
+//     // Spinner
+//     var spinner = function () {
+//         setTimeout(function () {
+//             if ($('#spinner').length > 0) {
+//                 $('#spinner').removeClass('show');
+//             }
+//         }, 1);
+//     };
+//     spinner();
+//
+//     // Initiate the wowjs
+//     new WOW().init();
+//
+//     // Navbar on scrolling
+//    $(window).scroll(function () {
+//         if ($(this).scrollTop() > 100) {
+//             $('.navbar').fadeIn('slow').css('display', 'flex');
+//         } else {
+//             $('.navbar').fadeOut('slow').css('display', 'none');
+//         }
+//     });
+//
+//     // Smooth scrolling on the navbar links
+//     $(".navbar-nav a").on('click', function (event) {
+//         if (this.hash !== "") {
+//             event.preventDefault();
+//
+//             $('html, body').animate({
+//                 scrollTop: $(this.hash).offset().top - 45
+//             }, 1500, 'easeInOutExpo');
+//
+//             if ($(this).parents('.navbar-nav').length) {
+//                 $('.navbar-nav .active').removeClass('active');
+//                 $(this).closest('a').addClass('active');
+//             }
+//         }
+//     });
+//
+//     // Back to top button
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 300) {
+//             $('.back-to-top').fadeIn('slow');
+//         } else {
+//             $('.back-to-top').fadeOut('slow');
+//         }
+//     });
+//     $('.back-to-top').click(function () {
+//         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+//         return false;
+//     });
+//
+//     // Typed Initiate
+//     if ($('.typed-text-output').length == 1) {
+//         var typed_strings = $('.typed-text').text();
+//         var typed = new Typed('.typed-text-output', {
+//             strings: typed_strings.split(', '),
+//             typeSpeed: 100,
+//             backSpeed: 20,
+//             smartBackspace: false,
+//             loop: true
+//         });
+//     }
+//
+//     // Mode sombre
+//     const getStoredTheme = () => localStorage.getItem('theme');
+//     const setStoredTheme = theme => localStorage.setItem('theme', theme);
+//
+//     const getPreferredTheme = () => {
+//         const storedTheme = getStoredTheme();
+//         if (storedTheme) {
+//             return storedTheme;
+//         }
+//
+//         return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+//     };
+//
+//     const setTheme = theme => {
+//         if (theme === 'auto') {
+//             document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+//         } else {
+//             document.documentElement.setAttribute('data-bs-theme', theme);
+//         }
+//
+//         toggleBackgroundClass(theme); // Bascule les classes de fond selon le thème
+//         toggleNavbarClass(theme); // Bascule la classe de la navbar selon le thème
+//     };
+//
+//     // Bascule entre bg-light et bg-dark pour les éléments de fond
+//     const toggleBackgroundClass = (theme) => {
+//         const elements = document.querySelectorAll('.bg-light, .bg-dark'); // Sélectionne tous les éléments avec les classes 'bg-light' ou 'bg-dark'
+//
+//         elements.forEach((element) => {
+//             if (theme === 'dark') {
+//                 element.classList.remove('bg-light');
+//                 element.classList.add('bg-dark'); // Ajoute la classe 'bg-dark' en mode sombre
+//             } else {
+//                 element.classList.remove('bg-dark');
+//                 element.classList.add('bg-light'); // Remet 'bg-light' en mode clair
+//             }
+//         });
+//     };
+//
+//     // Bascule entre navbar-light et navbar-dark pour la navbar
+//     const toggleNavbarClass = (theme) => {
+//         const navbar = document.querySelector('.navbar'); // Sélectionne la navbar par sa classe
+//
+//         if (navbar) {
+//             if (theme === 'dark') {
+//                 navbar.classList.remove('navbar-light');
+//                 navbar.classList.add('navbar-dark'); // En mode sombre, on utilise 'navbar-dark'
+//             } else {
+//                 navbar.classList.remove('navbar-dark');
+//                 navbar.classList.add('navbar-light'); // En mode clair, on utilise 'navbar-light'
+//             }
+//         }
+//     };
+//
+//     setTheme(getPreferredTheme());
+//
+//     const showActiveTheme = (theme, focus = false) => {
+//         const themeSwitcher = document.querySelector('#bd-theme');
+//
+//         if (!themeSwitcher) {
+//             return;
+//         }
+//
+//         const themeSwitcherText = document.querySelector('#bd-theme-text');
+//         const activeThemeIcon = document.querySelector('.theme-icon-active use');
+//         const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
+//         const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href');
+//
+//         document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+//             element.classList.remove('active');
+//             element.setAttribute('aria-pressed', 'false');
+//         });
+//
+//         btnToActive.classList.add('active');
+//         btnToActive.setAttribute('aria-pressed', 'true');
+//         activeThemeIcon.setAttribute('href', svgOfActiveBtn);
+//         const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+//         themeSwitcher.setAttribute('aria-label', themeSwitcherLabel);
+//
+//         if (focus) {
+//             themeSwitcher.focus();
+//         }
+//     };
+//
+//     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+//         const storedTheme = getStoredTheme();
+//         if (storedTheme !== 'light' && storedTheme !== 'dark') {
+//             setTheme(getPreferredTheme());
+//         }
+//     });
+//
+//     window.addEventListener('DOMContentLoaded', () => {
+//         showActiveTheme(getPreferredTheme());
+//
+//         document.querySelectorAll('[data-bs-theme-value]')
+//             .forEach(toggle => {
+//                 toggle.addEventListener('click', () => {
+//                     const theme = toggle.getAttribute('data-bs-theme-value');
+//                     setStoredTheme(theme);
+//                     setTheme(theme);
+//                     showActiveTheme(theme, true);
+//                 });
+//             });
+//     });
+//
+//     /*document.getElementById('show-timeline-icon').addEventListener('click', function() {
+//         var timelineSection = document.getElementById('timeline-section');
+//         if (timelineSection.style.display === "none" || timelineSection.style.display === "") {
+//             timelineSection.style.display = "block";
+//         } else {
+//             timelineSection.style.display = "none";
+//         }
+//     });*/
+// })(jQuery);
 (function ($) {
     "use strict";
 
@@ -12,12 +192,12 @@
     };
     spinner();
 
-    // Initiate the wowjs
+    // Initiate wowjs
     new WOW().init();
 
     // Navbar on scrolling
-   $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
             $('.navbar').fadeIn('slow').css('display', 'flex');
         } else {
             $('.navbar').fadeOut('slow').css('display', 'none');
@@ -29,10 +209,21 @@
         if (this.hash !== "") {
             event.preventDefault();
 
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
+            // Redirection vers la page principale si on est sur une page secondaire
+            var currentUrl = window.location.href.split('#')[0]; // URL sans le hash
+            var mainPageUrl = "https://www.jeromesauvage.fr"; // URL de la page principale
 
+            // Si la page actuelle n'est pas la page principale, redirection complète vers la page principale
+            if (currentUrl !== mainPageUrl) {
+                window.location.href = mainPageUrl + this.hash; // Redirige vers l'ancre sur la page principale
+            } else {
+                // Si on est déjà sur la page principale, faire un scrolling doux vers l'ancre
+                $('html, body').animate({
+                    scrollTop: $(this.hash).offset().top - 45
+                }, 1500, 'easeInOutExpo');
+            }
+
+            // Mettre à jour la classe active dans le menu de navigation
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
                 $(this).closest('a').addClass('active');
@@ -65,117 +256,38 @@
         });
     }
 
-    // Mode sombre
-    const getStoredTheme = () => localStorage.getItem('theme');
-    const setStoredTheme = theme => localStorage.setItem('theme', theme);
-
-    const getPreferredTheme = () => {
-        const storedTheme = getStoredTheme();
-        if (storedTheme) {
-            return storedTheme;
-        }
-
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    };
-
-    const setTheme = theme => {
-        if (theme === 'auto') {
-            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
-        } else {
-            document.documentElement.setAttribute('data-bs-theme', theme);
-        }
-
-        toggleBackgroundClass(theme); // Bascule les classes de fond selon le thème
-        toggleNavbarClass(theme); // Bascule la classe de la navbar selon le thème
-    };
-
-    // Bascule entre bg-light et bg-dark pour les éléments de fond
-    const toggleBackgroundClass = (theme) => {
-        const elements = document.querySelectorAll('.bg-light, .bg-dark'); // Sélectionne tous les éléments avec les classes 'bg-light' ou 'bg-dark'
-
-        elements.forEach((element) => {
-            if (theme === 'dark') {
-                element.classList.remove('bg-light');
-                element.classList.add('bg-dark'); // Ajoute la classe 'bg-dark' en mode sombre
-            } else {
-                element.classList.remove('bg-dark');
-                element.classList.add('bg-light'); // Remet 'bg-light' en mode clair
-            }
-        });
-    };
-
-    // Bascule entre navbar-light et navbar-dark pour la navbar
-    const toggleNavbarClass = (theme) => {
-        const navbar = document.querySelector('.navbar'); // Sélectionne la navbar par sa classe
-
-        if (navbar) {
-            if (theme === 'dark') {
-                navbar.classList.remove('navbar-light');
-                navbar.classList.add('navbar-dark'); // En mode sombre, on utilise 'navbar-dark'
-            } else {
-                navbar.classList.remove('navbar-dark');
-                navbar.classList.add('navbar-light'); // En mode clair, on utilise 'navbar-light'
-            }
-        }
-    };
-
-    setTheme(getPreferredTheme());
-
-    const showActiveTheme = (theme, focus = false) => {
-        const themeSwitcher = document.querySelector('#bd-theme');
-
-        if (!themeSwitcher) {
-            return;
-        }
-
-        const themeSwitcherText = document.querySelector('#bd-theme-text');
-        const activeThemeIcon = document.querySelector('.theme-icon-active use');
-        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
-        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href');
-
-        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-            element.classList.remove('active');
-            element.setAttribute('aria-pressed', 'false');
-        });
-
-        btnToActive.classList.add('active');
-        btnToActive.setAttribute('aria-pressed', 'true');
-        activeThemeIcon.setAttribute('href', svgOfActiveBtn);
-        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
-        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel);
-
-        if (focus) {
-            themeSwitcher.focus();
-        }
-    };
-
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-        const storedTheme = getStoredTheme();
-        if (storedTheme !== 'light' && storedTheme !== 'dark') {
-            setTheme(getPreferredTheme());
-        }
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
     });
 
-    window.addEventListener('DOMContentLoaded', () => {
-        showActiveTheme(getPreferredTheme());
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
 
-        document.querySelectorAll('[data-bs-theme-value]')
-            .forEach(toggle => {
-                toggle.addEventListener('click', () => {
-                    const theme = toggle.getAttribute('data-bs-theme-value');
-                    setStoredTheme(theme);
-                    setTheme(theme);
-                    showActiveTheme(theme, true);
-                });
-            });
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
 
-    /*document.getElementById('show-timeline-icon').addEventListener('click', function() {
-        var timelineSection = document.getElementById('timeline-section');
-        if (timelineSection.style.display === "none" || timelineSection.style.display === "") {
-            timelineSection.style.display = "block";
-        } else {
-            timelineSection.style.display = "none";
-        }
-    });*/
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: true,
+        loop: true,
+    });
+
 })(jQuery);
